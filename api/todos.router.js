ab346bfda37bad46fa35a4218cef9c6b9b6df693
Francisco J. López <frandufour@protonmail.com>
2021-11-14 19:40:14 +0000

    add todos.router

---
+++ api/todos.router.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+router.get('', (req, res) => {
+    return res.json([])
+})
+
+router.get('/:id', (req, res) => {
+    return res.json([])
+})
+
+router.post('', (req, res) => {
+    return res.json([])
+})
+
+router.delete('/:id', (req, res) => {
+    return res.json([])
+})
+
+router.patch('/:id', (req, res) => {
+    return res.json([])
+})
+
+module.exports();
--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const express = require('express');
 const app = express();
 
+const todosRouter = require('./api/todos.router')
+
 // app.get('/', (req, res) => {
 //     return res.send('hola');
 // }) 
 
-app.get('/todos', (req, res) => {
-    return res.json([])
-})
-
-app.get('/todos/:id', (req, res) => {
-    return res.json([])
-})
-
-app.post('/todos', (req, res) => {
-    return res.json([])
-})
-
-app.delete('/todos/:id', (req, res) => {
-    return res.json([])
-})
-
-app.patch('/todos/:id', (req, res) => {
-    return res.json([])
-})
+app.use('/todos', todosRouter)
 
 app.listen(5000, (err) => {
     if (!err) {
