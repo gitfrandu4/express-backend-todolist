b9b0745776dd5a6488ddfa912c5f160cf7eb1e68
Francisco J. López <frandufour@protonmail.com>
2021-11-15 03:28:28 +0000

    add todos.model.js

---
+++ src/api/todos.model.js
@@ -0,0 +1,24 @@
+// 1. Importamos mongoose
+const mongoose = require('mongoose');
+
+// 2. Aquí definimos el esquema de un todo
+const schema = new mongoose.Schema({
+    title: {
+        required: true,
+        type: String,
+        minlength: 5,
+        maxlength: 255,
+    },
+    completed: {
+        required: true,
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true})
+
+// 3. 
+// todo => nombre del recurso que podemos guardar en la bd
+const model = mongoose.model('todo', schema)
+
+// 4. Exportamos el modelo
+module.exports = model;
